e6ff4fee046230e26fd394c8c4bc3582fed143f3
dadddd-aaa1112 <74870737+dadddd-aaa1112@users.noreply.github.com>
2021-12-11 09:29:10 +0300

    add questions ana right ansers add questions ana right ansers

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -2,17 +2,27 @@ import React from "react";
 import classes from "./ActiveQuiz.module.css";
 import AnswersList from "./AnswersList/AnswersList";
 
-const ActiveQuiz = (props) => (
-  <div>
-    <p>
-      <span>
-        <strong>2.</strong>&nbsp; how do you do?
-      </span>
-      <small>4 of 12</small>
-    </p>
+const ActiveQuiz = (props) => {
+  return (
+    <div className={classes.ActiveQuiz}>
+      <p className={classes.Question}>
+        <span>
+          <strong>{props.answerNumber}.</strong>&nbsp;
+          {props.question}
+        </span>
 
-    <AnswersList answers={props.answers} />
-  </div>
-);
+        <small>
+          {props.answerNumber} из {props.quizLength}
+        </small>
+      </p>
+
+      <AnswersList
+        state={props.state}
+        answers={props.answers}
+        onAnswerClick={props.onAnswerClick}
+      />
+    </div>
+  );
+};
 
 export default ActiveQuiz;
--- src/components/ActiveQuiz/AnswersList/AnswersList.js
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -2,12 +2,21 @@ import React from "react";
 import classes from "./AnswersList.module.css";
 import AnswerItem from "./AnswerItem/AnswerItem";
 
-const AnswersList = (props) => (
-  <ul>
-    {props.answers.map((answer, index) => {
-      return <AnswerItem key={index} answer={answer} />;
-    })}
-  </ul>
-);
+const AnswersList = (props) => {
+  return (
+    <ul className={classes.AnswersList}>
+      {props.answers.map((answer, index) => {
+        return (
+          <AnswerItem
+            key={index}
+            answer={answer}
+            onAnswerClick={props.onAnswerClick}
+            state={props.state ? props.state[answer.id] : null}
+          />
+        );
+      })}
+    </ul>
+  );
+};
 
 export default AnswersList;
